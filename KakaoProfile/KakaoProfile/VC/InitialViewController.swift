bc22f19dc0d43f01e038123a48c7fca7ccb9a4d8
Tree.JI <tree.ji@kakaocorp.com>
2021-01-11 16:52:32 +0900

    Init Project_KakaoProfile

---
+++ KakaoProfile/KakaoProfile/VC/InitialViewController.swift
@@ -0,0 +1,18 @@
+//
+//  InitialViewController.swift
+//  KakaoProfile
+//
+//  Created by 지현우 on 2021/01/11.
+//
+import UIKit
+import Foundation
+
+class InitialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
---
+++ KakaoProfile/KakaoProfile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KakaoProfile
+//
+//  Created by 지현우 on 2021/01/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
