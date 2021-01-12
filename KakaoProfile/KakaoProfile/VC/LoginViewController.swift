e491e628c137951d82442a7e7b0f8cc4b96d58c3
Tree.JI <tree.ji@kakaocorp.com>
2021-01-12 14:32:16 +0900

    Implement EditProfile, Use Delegate, Implement UIImagePickerController

--- KakaoProfile/KakaoProfile/VC/LoginViewController.swift
+++ KakaoProfile/KakaoProfile/VC/LoginViewController.swift
@@ -6,25 +6,63 @@ class LoginViewController: UIViewController {
 
     var nameText: String?
     var descriptionText: String?
+    weak var delegate: ProfileDelegate?
+    
     @IBOutlet weak var profileImageView: UIImageView!
     @IBOutlet weak var cameraButton: UIButton!
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var descriptionTextField: UITextField!
     
+    lazy var imagePicker : UIImagePickerController = {
+        let picker: UIImagePickerController = UIImagePickerController()
+        picker.sourceType = .photoLibrary
+        picker.delegate = self
+        return picker
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("1")
+        initImageView()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        print("2")
         self.nameTextField.text = nameText
         self.descriptionTextField.text = descriptionText
     }
+    private func initImageView(){
+        self.profileImageView.contentMode = .scaleAspectFill
+        self.profileImageView.roundView(by: 10)
+        self.profileImageView.setBorder(thick: 0.5, color: UIColor.black.cgColor)
+    }
     
+    @IBAction func tappedCameraButton(_ sender: Any) {
+        self.present(imagePicker, animated: true, completion: nil)
+    }
     
+    @IBAction func tappedCancelButton(_ sender: Any) {
+        self.presentingViewController?.dismiss(animated: true)
+    }
     
+    @IBAction func tappedDoneButton(_ sender: Any) {
+       
+        delegate?.editProfile(image: self.profileImageView.image, name: self.nameTextField.text ?? "이름", description: self.descriptionTextField.text ?? "설명")
+        self.presentingViewController?.dismiss(animated: true, completion: nil)
+    }
+    
+}
 
-
+extension LoginViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    //취소 시
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    //이미지 선택 시
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image: UIImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+            self.profileImageView.image = image
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
 }
--- KakaoProfile/KakaoProfile/VC/ProfileViewController.swift
+++ KakaoProfile/KakaoProfile/VC/ProfileViewController.swift
@@ -2,6 +2,10 @@
 import UIKit
 import Foundation
 
+protocol ProfileDelegate: class {
+    func editProfile(image: UIImage?, name: String, description: String)
+}
+
 class ProfileViewController: UIViewController {
     
     @IBOutlet weak var profileImageView: UIImageView!
@@ -12,6 +16,11 @@ class ProfileViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setProfile()
+        initImageView()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
     }
     
     private func setProfile(){
@@ -19,19 +28,49 @@ class ProfileViewController: UIViewController {
         self.descriptionLabel.text = "ㅋㅋㅋㅋㅋ"
     }
     
+    private func initImageView(){
+        self.profileImageView.contentMode = .scaleAspectFill
+        self.profileImageView.roundView(by: 10)
+        self.profileImageView.setBorder(thick: 0.5, color: UIColor.black.cgColor)
+    }
+    
     @IBAction func tappedEditButton(_ sender: Any) {
         self.nameLabel.textColor = .blue
         self.nameLabel.backgroundColor = .yellow
         self.nameLabel.alpha = 0.5
         self.descriptionLabel.text = "크루미션"
-    }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if let destination = segue.destination as? LoginViewController{
-            destination.nameText = self.nameLabel.text ?? "name"
-            destination.descriptionText = self.descriptionLabel.text ?? "description"
+        
+        //방법2
+//        performSegue(withIdentifier: "ProfileToLogin", sender: nil)
+        
+        //방법3
+        if let loginVC = self.storyboard?.instantiateViewController(withIdentifier: "LoginVC") as? LoginViewController{
+            loginVC.delegate = self
+            loginVC.nameText = self.nameLabel.text
+            loginVC.descriptionText = self.descriptionLabel.text
+            present(loginVC, animated: true, completion: nil)
         }
     }
     
+    //방법 1, 2
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        if segue.identifier == "ProfileToLogin"{
+//            if let loginVC = segue.destination as? LoginViewController{
+//                loginVC.delegate = self
+//                loginVC.nameText = self.nameLabel.text ?? "name"
+//                loginVC.descriptionText = self.descriptionLabel.text ?? "description"
+//            }
+//        }
+//
+//    }
+    
+}
 
+extension ProfileViewController: ProfileDelegate{
+    func editProfile(image: UIImage?, name: String, description: String) {
+        print("name:\(name) , des: \(description)")
+        self.profileImageView.image = image
+        self.nameLabel.text = name
+        self.descriptionLabel.text = description
+    }
 }
