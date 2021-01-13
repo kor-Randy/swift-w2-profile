812659f6dea3a30085eb76986b608dc3fecb34bd
Tree.JI <tree.ji@kakaocorp.com>
2021-01-13 13:30:13 +0900

    Add setWebView()

--- KakaoProfile/KakaoProfile/VC/MainViewController.swift
+++ KakaoProfile/KakaoProfile/VC/MainViewController.swift
@@ -12,18 +12,25 @@ import WebKit
 class MainViewController: UIViewController {
     @IBOutlet var webView: WKWebView!
     private let url = URL(string: "https://m.daum.net")
-    private lazy var request = URLRequest(url: url!)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.navigationBar.isHidden = false
         self.navigationController?.title = "뉴스"
-        self.webView.load(self.request)
+        setWebView()
     }
-    
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.showToast(vc: self, msg: "로그인되었습니다.", sec: 1.0)
-        
+    }
+
+    private func setWebView() {
+        if let url = self.url{
+            let request = URLRequest(url: url)
+            self.webView.load(request)
+        }else{
+            showToast(vc: self, msg: "URL에 접근할 수 없습니다.", sec: 1.0)
+        }
     }
 }
